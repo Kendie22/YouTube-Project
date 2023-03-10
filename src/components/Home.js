029d337ba9908640816debb6d784c5f7bd4528cf
Kerridene Small <Kerridenesmall@gmail.com>
2023-03-10 17:06:54 -0500

    ??

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,6 +10,7 @@ export function Home() {
     // const navigate = useNavigate();
 
     useEffect(() => {
+
         getAllVideos()
             .then((response) => {
                 setVideos(response);
