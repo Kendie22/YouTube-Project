e1a2a730ddbc1681a07428e743e40214b99ca651
Kerridene Small <Kerridenesmall@gmail.com>
2023-03-10 11:13:46 -0500

    adding return to nav-bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,3 +1,12 @@
+import "./NavBar.css";
 export function NavBar() {
+    return (
+        <header>
+            <article>
+                <h1>
 
+                </h1>
+            </article>
+        </header>
+    )
 }
