4ad4b36608fe2f6a651fe75bbaaf0b14988406d3
Kerridene Small <Kerridenesmall@gmail.com>
2023-03-10 11:34:37 -0500

    css

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,4 +1,20 @@
 import "./NavBar.css";
 export function NavBar() {
+    return (
+        <header>
+            <article>
+                <h1>
+                    YouTube
+                </h1>
+            </article>
+            <nav>
+                <ul>
+                    <li>
 
+                    </li>
+
+                </ul>
+            </nav>
+        </header>
+    )
 }
