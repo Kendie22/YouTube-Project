c16c25390d860bc6d303b2bcd7a1876e15b6a6b5
Kerridene Small <Kerridenesmall@gmail.com>
2023-03-10 15:54:35 -0500

    fixing Home

---
+++ src/components/Home.js
@@ -0,0 +1,21 @@
+import { useState, useEffect } from "react";
+import { Link, useNavigate, useParams } from "react-router-dom";
+import { getAllVideos } from "scr/api/fetch.js";
+
+export function Home() {
+    const [videos, setVideos] = useState({});
+    // const [loadingError, setLoadingError] = useState(false);
+    // // console.log(useParams());
+    // const { id } = useParams(); //useParams gives us access to the parameters we set in our paths in our routing
+    // const navigate = useNavigate();
+
+    useEffect(() => {
+        getAllVideos()
+            .then((response) => {
+                setVideos(response);
+
+
+            }, []);
+
+    })
+}
