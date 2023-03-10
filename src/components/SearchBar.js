f2eae3efef8983be7f0b6f4255dd4385080e8075
Kerridene Small <Kerridenesmall@gmail.com>
2023-03-10 15:29:22 -0500

    nav-bar css some search bar work

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,3 +1,27 @@
+import { useState, useEffect } from "react";
+
+
 export function SearchBar() {
 
+    function videoSearch() {
+
+    };
+
+
+    return (
+
+        <>
+            <div className="search">
+                <form>
+                    <input type="text"></input>
+                    <button onClick={videoSearch}>Search</button>
+                </form>
+            </div>
+            <div className="Videos">
+                {/* map */}
+            </div>
+        </>
+
+
+    )
 }
