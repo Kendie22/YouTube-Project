731d5e5d5780afc48c45955ddffb047a6c003c84
Taylor Brooks <tybrooks01@icloud.com>
2023-03-10 12:19:57 -0500

    YouTube-Project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+// import { About } from "./components/About";
+import { Home } from "./components/Home";
+import { useEffect } from "react";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const testyouTubeAPI = () => {
+    console.log(process.env.REACT_APP_YOUTUBE_API_KEY);
+  };
+  useEffect(() => {
+    testyouTubeAPI();
+  });
+
+  return;
+  <Home />;
 }
 
 export default App;
---
+++ src/components/About.js
@@ -0,0 +1,20 @@
+export default function About() {
+  const projectBio = "This project was built by a team of 3 developers. ";
+
+  const shaqFredBio = "";
+  const kerrideneBio =
+    "Kerridene is a dog mom, an avid gardner & enjoys spending time with her family & friends.";
+  const tayBrooksBio =
+    "Taylor is an aspiring engineer, mother & lover of crafts.";
+
+  return (
+    <div>
+      <h2>About Our Project</h2>
+      <p>projectBio</p>
+      <h2>About the Developers</h2>
+      <p>shaqFredBio</p>
+      <p>kerrideneBio</p>
+      <p>tayBrooksBio</p>
+    </div>
+  );
+}
