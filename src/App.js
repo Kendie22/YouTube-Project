d91957c66c40a1b34441ff9cc4fbfeb1570bb60e
Taylor Brooks <tybrooks01@icloud.com>
2023-03-12 15:51:09 -0400

    about page rendering & path working

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { SingleVideoPage } from "./components/SingleVideoPage";
 import { SearchBar } from "./components/SearchBar";
 import { useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import './App.css';
+import "./App.css";
 
 function App() {
   const testYouTubeApi = () => {
@@ -23,7 +23,7 @@ function App() {
           <Route path="/" exact element={<Home />} />
           <Route path="/video/:id" element={<SingleVideoPage />} />
           <Route path="/search/:id" element={<SearchBar />} />
-          {/* <Route path="/about" element={<About />} /> */}
+          <Route path="/about" element={<About />} />
         </Routes>
       </BrowserRouter>
     </div>
