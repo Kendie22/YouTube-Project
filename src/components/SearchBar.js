cced85bb5b0c1ca70b2690611684a71312e1cbf2
Taylor Brooks <tybrooks01@icloud.com>
2023-03-12 16:14:03 -0400

    working on searchbar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,3 +1,23 @@
+import { useState } from "react";
+import React from "react";
+// import { Search } from "@mui/icons-material";
 
-export function SearchBar() {}
+function filter() {
+  URL.filter(((names) => names.includes()).map);
+}
 
+export function SearchBar() {
+  const [input, setUserInput] = useState("");
+
+  function handleUserInput(event) {
+    event.preventDefault();
+    setUserInput(event.target.value);
+  }
+
+  return;
+  <main>
+    <form>onSubmit={handleUserInput}</form>
+
+    <button type="submit">Search</button>
+  </main>;
+}
